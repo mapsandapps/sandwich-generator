7cdea41c620dde9c561bfac9fa727c079833a8d6
Shawn Taylor <shawn@mapsandapps.net>
2020-12-05 20:15:09 -0500

    Make puzzle from grid

---
+++ generate-grid.js
@@ -0,0 +1,169 @@
+// @ts-nocheck
+const SIZE = 5
+
+var initialRow = _.times(SIZE, _.constant(0))
+
+const digits = _.map(Array(SIZE), (item, i) => {
+  return i + 1
+})
+
+var grid = _.times(SIZE, () => _.clone(initialRow))
+
+const setRandomDigit = (i, j) => {
+  return _.random(1, SIZE)
+}
+
+const getRowDigits = (i, j) => {
+  // TODO: ignore 0's
+  var rowDigits = _.clone(grid[i])
+  rowDigits.splice(j, 1)
+
+  return rowDigits
+}
+
+const getColDigits = (i, j) => {
+  // TODO: ignore 0's
+  var colDigits = _.map(_.clone(grid), (item) => {
+    return item[j]
+  })
+  colDigits.splice(i, 1)
+
+  return colDigits
+}
+
+const getAvailableDigits = (i, j) => {
+  var availableDigits = _.clone(digits)
+  _.pullAll(availableDigits, getRowDigits(i, j))
+  _.pullAll(availableDigits, getColDigits(i, j))
+
+  return availableDigits
+}
+
+const findProblemDigit = (i, j) => {
+  // get all available digits for the column
+  var availableColDigits = _.pullAll(_.clone(digits), getColDigits(i, j))
+
+  // if one of those is in the row, replace that digit
+  var l = _.indexOf(getRowDigits(i, j), availableColDigits[0])
+  // grid[i][l] = '.'
+  // printGrid()
+  setDigit(i, l, true)
+}
+
+const setDigit = (i, j, isRecursing = false) => {
+  if (i < 0 || j < 0) {
+    return
+  }
+
+  var availableDigits = getAvailableDigits(i, j)
+
+  if (availableDigits.length < 1) {
+    findProblemDigit(i, j)
+    grid[i][j] = '.'
+  } else {
+    if (isRecursing) {
+      // give the digit a chance to be '.', to back out of a hole
+      const randomNumber = _.random(availableDigits.length)
+      if (randomNumber === availableDigits.length) {
+        // TODO: recurse
+        console.log('setting position ', i, ',', j, 'to .')
+        findProblemDigit(i, j)
+        grid[i][j] = '.'
+      } else {
+        const digitToSet = availableDigits[randomNumber]
+        console.log('setting position ', i, ',', j, 'to ', digitToSet)
+        grid[i][j] = digitToSet
+      }
+    } else {
+      const digitToSet = availableDigits[_.random(availableDigits.length - 1)]
+      grid[i][j] = digitToSet
+    }
+  }
+
+  printGrid()
+}
+
+const findFirstDot = () => {
+  var firstDotI
+  var firstDotJ
+  _.map(grid, (row, i) => {
+    var j = _.indexOf(row, '.')
+    if (!firstDotI && !firstDotJ && j > -1) {
+      firstDotI = i
+      firstDotJ = j
+      console.log(i, j)
+    }
+  })
+
+  if (firstDotI >= 0 && firstDotJ >= 0) {
+    // try to set the dot to a digit, in case its blockers have already been cleared, if not, that function will recurse
+    setDigit(firstDotI, firstDotJ)
+  }
+}
+
+const setGrid = () => {
+  grid = _.times(SIZE, () => _.clone(initialRow))
+  _.map(grid, (row, i) => {
+    _.map(row, (item, j) => {
+      setDigit(i, j)
+    })
+  })
+}
+
+const printGrid = () => {
+  var html = ''
+  var json = {
+    colSums: getColSums(),
+    rowSums: getRowSums(),
+    grid: grid
+  }
+
+  html += '<div>&nbsp;&nbsp;';
+  _.each(getColSums(), (sum) => {
+    if (sum < 10) {
+      html += '&nbsp;&nbsp;' + sum
+    } else {
+      html += '&nbsp;' + sum
+    }
+  });
+  html += '</div>'
+
+  _.map(grid, (row, i) => {
+    html += '<div>'
+    var sum = getRowSum(i);
+    if (sum < 10) {
+      html += '&nbsp;' + sum;
+    } else {
+      html += sum;
+    }
+    html += '&nbsp;&nbsp;'
+    html += row.join('&nbsp;&nbsp;')
+    html += '</div>'
+  });
+
+  html += '<br><br><div>'
+  json.size = SIZE
+  html += JSON.stringify(json)
+
+  html += '</div>'
+
+  document.getElementById('grid').innerHTML = html
+}
+
+const helloWorld = () => {
+  document.getElementById('grid').innerHTML = 'hello world'
+}
+
+const generate = () => {
+  setGrid()
+}
+
+document.getElementById('generate').onclick = generate
+document.getElementById('findDot').onclick = findFirstDot
+generate()
+
+
+// TODO: maybe instead of this, it should fill all with numbers (random numbers instead of dots)
+// then check if the grid is good
+// if bad number is found in grid, try swapping it with another number up or to the left of it
+// repeat
---
+++ generate-puzzle.js
@@ -0,0 +1,127 @@
+// @ts-nocheck
+
+// {'colSums':[2,0,0,0,0],'rowSums':[1,2,2,1,6],'grid':[[3,4,1,5,2],[1,5,2,4,3],[4,2,5,3,1],[2,3,4,1,5],[5,1,3,2,4]]}
+
+var completeGrid
+
+const difficulty = 0.7
+
+const makePuzzle = () => {
+  completeGrid = JSON.parse(document.getElementById('textarea').value)
+  console.log(completeGrid)
+  console.log(_.random(true))
+
+  completeGrid.colSumsFixed = []
+  completeGrid.rowSumsFixed = []
+  completeGrid.gridFixed = []
+  completeGrid.difficulty = difficulty
+  completeGrid.difficultyVersion = 0.1
+
+  _.map(completeGrid.colSums, sum => {
+    if (_.random(true) > difficulty) {
+      completeGrid.colSumsFixed.push(true)
+    } else {
+      completeGrid.colSumsFixed.push(false)
+    }
+  })
+
+  _.map(completeGrid.rowSums, sum => {
+    if (_.random(true) > difficulty) {
+      completeGrid.rowSumsFixed.push(true)
+    } else {
+      completeGrid.rowSumsFixed.push(false)
+    }
+  })
+
+  _.map(completeGrid.grid, (row, i) => {
+    completeGrid.gridFixed.push([])
+    _.map(row, item => {
+      if (_.random(true) > difficulty) {
+        completeGrid.gridFixed[i].push(true)
+      } else {
+        completeGrid.gridFixed[i].push(false)
+      }
+    })
+  })
+
+  printPuzzle()
+}
+
+const printPuzzle = () => {
+  var html = '<div>Solution:</div>'
+
+  html += '<div>&nbsp;&nbsp;';
+  _.each(completeGrid.colSums, (sum) => {
+    if (sum < 10) {
+      html += '&nbsp;&nbsp;' + sum
+    } else {
+      html += '&nbsp;' + sum
+    }
+  });
+  html += '</div>'
+
+  _.map(completeGrid.grid, (row, i) => {
+    html += '<div>'
+    var sum = completeGrid.rowSums[i];
+    if (sum < 10) {
+      html += '&nbsp;' + sum;
+    } else {
+      html += sum;
+    }
+    html += '&nbsp;&nbsp;'
+    html += row.join('&nbsp;&nbsp;')
+    html += '</div>'
+  });
+
+  html = '<div>Puzzle:</div>'
+  html += '<div>&nbsp;&nbsp;';
+  _.each(completeGrid.colSums, (sum, j) => {
+    if (completeGrid.colSumsFixed[j]) {
+      if (sum < 10) {
+        html += '&nbsp;&nbsp;' + sum
+      } else {
+        html += '&nbsp;' + sum
+      }
+    } else {
+      html += '&nbsp;&nbsp;&nbsp;'
+    }
+  });
+  html += '</div>'
+
+  _.map(completeGrid.grid, (row, i) => {
+    html += '<div>'
+    var sum = completeGrid.rowSums[i];
+    if (completeGrid.rowSumsFixed[i]) {
+      if (sum < 10) {
+        html += '&nbsp;' + sum;
+      } else {
+        html += sum;
+      }
+    } else {
+      html += '&nbsp;&nbsp;'
+    }
+    _.map(row, (item, j) => {
+      html += '&nbsp;&nbsp;'
+      if (completeGrid.gridFixed[i][j]) {
+        const value = completeGrid.grid[i][j]
+        if (value >= completeGrid.size - 1) {
+          html += 'X'
+        } else {
+          html += completeGrid.grid[i][j]
+        }
+      } else {
+        html += '_'
+      }
+    })
+    html += '</div>'
+  });
+
+  html += '<br><br><div>'
+  html += JSON.stringify(completeGrid)
+
+  html += '</div>'
+
+  document.getElementById('puzzle').innerHTML = html
+}
+
+document.getElementById('makePuzzle').onclick = makePuzzle
